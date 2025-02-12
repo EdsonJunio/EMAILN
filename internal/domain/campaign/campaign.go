6e1155bd5eb6a078c99ae11e698b769f51b4e65e
EdsonJunio <edsonbh97@gmail.com>
2025-02-12 17:05:27 -0300

    Feat: Constructor

--- internal/domain/campaign/campaign.go
+++ internal/domain/campaign/campaign.go
@@ -2,7 +2,7 @@ package campaign
 
 import "time"
 
-type Contacts struct {
+type Contact struct {
 	Email string
 }
 
@@ -11,5 +11,21 @@ type Campaign struct {
 	Name      string
 	CreatedOn time.Time
 	Content   string
-	Contacts  []Contacts
+	Contacts  []Contact
+}
+
+func NewCampaign(name string, content string, emails []string) *Campaign {
+
+	contacts := make([]Contact, len(emails))
+	for index, email := range emails {
+		contacts[index].Email = email
+	}
+
+	return &Campaign{
+		ID:        "1",
+		Name:      name,
+		Content:   content,
+		CreatedOn: time.Now(),
+		Contacts:  contacts,
+	}
 }
