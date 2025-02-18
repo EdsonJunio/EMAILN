5392983aa3763faaafa73ca0ec04c0ceb411835d
EdsonJunio <edsonbh97@gmail.com>
2025-02-17 21:57:46 -0300

    Revert "Feat: go get github.com/go-chi/render / retornando json"
    This reverts commit 075282fb7b54e94de5b6e94eebf80834a3c8abb7.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,15 +2,13 @@ package main
 
 import (
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/render"
 	"net/http"
 )
 
 func main() {
 	r := chi.NewRouter()
-	r.Get("/json", func(w http.ResponseWriter, r *http.Request) {
-		obj := map[string]string{"message": "sucess"}
-		render.JSON(w, r, obj)
+	r.Get("/", func(w http.ResponseWriter, request *http.Request) {
+		w.Write([]byte("Hello World!"))
 	})
 	http.ListenAndServe(":3000", r)
 }
